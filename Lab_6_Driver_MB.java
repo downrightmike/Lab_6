ea109ba93063b1ca38e798f0248aee5b6d69cd93
Mike <downrightmike@gmail.com>
2018-10-25 17:27:17 -0700

    start

---
+++ Lab_6_Driver_MB.java
@@ -0,0 +1,23 @@
+        /********************************************************************************************************************
+        * Lab_6_MB.java
+        * Author: Downrightmike
+        * CIS 131
+        * 
+        * 
+        ********************************************************************************************************************/
+        import java.util.Scanner;
+
+        public class Lab_6_Driver_MB {
+        private static Scanner keyboard = new Scanner(System.in);
+        
+        public static void main(String[] args) {           
+                final int empNo = 0;
+                final double payRate = 0.0;
+
+
+                System.out.println("Compiled.");
+          }
+    
+          
+
+        }
